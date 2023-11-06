feb5daf34b37bcd57263d00d7f96401fe7572926
renmuming <1652339654@qq.com>
2023-11-06 15:29:41 +0800

    计数器

--- vue3/src/view/Pinia.vue
+++ vue3/src/view/Pinia.vue
@@ -1,9 +1,13 @@
 <template>
-  计数器的值：{{ count }}
-  <button @click="increment">修改计数器的值</button>
+  <RouterLink to="/"><ElButton>返回</ElButton></RouterLink>
+  <div>
+    计数器的值：{{ count }}
+  <ElButton round @click="increment">修改计数器的值</ElButton>
+  </div>
 </template>
 
 <script lang="ts" setup>
+import { ElButton } from 'element-plus'
 import { storeToRefs } from 'pinia'
 import store from '../store/index'
 // 获取pinia状态管理中的count值
