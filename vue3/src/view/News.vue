ff7731ff4fcdd7aa338b222ccf13c0269475ec50
renmuming <1652339654@qq.com>
2023-11-06 15:29:35 +0800

    新闻

---
+++ vue3/src/view/News.vue
@@ -0,0 +1,8 @@
+<template>
+  <RouterLink to="/"><ElButton>返回</ElButton></RouterLink>
+  <div>新闻信息</div>
+</template>
+<script lang="ts" setup>
+import { ElButton } from 'element-plus'
+</script>
+<style lang="scss" scoped></style>
